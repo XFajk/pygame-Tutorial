2dca8881d4308ef666e77a16161a20eeaee9799f
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2022-11-09 21:54:59 +0000

    Added display to display stuff in any resolution

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import pygame
 
 pygame.init()
 
-window = pygame.display.set_mode((800,600))
+ZOOM = 1
+WS = (800,600)
+DS = (WS[0]/ZOOM,WS[1]/ZOOM)
+
+window = pygame.display.set_mode()
+display = pygame.Surface(DS)
+
 clock = pygame.time.Clock()
 
 # variables
@@ -14,9 +20,9 @@ while running:
 
 	obdlznik = pygame.Rect(x,200,100,50)
 
-	window.fill((255,255,255))
+	display.fill((255,255,255))
 
-	pygame.draw.rect(window,(255,0,0),obdlznik)
+	pygame.draw.rect(display,(255,0,0),obdlznik)
 	x += 1
 
 	pygame.display.update()
@@ -26,6 +32,8 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False	
 
+	surf = pygame.transform.scale(display,WS)
+	window.blit(surf, (0,0))
 	clock.tick(60)
 	pygame.display.set_caption(f"game window FPS: {int(clock.get_fps())}")
 
